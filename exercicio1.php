85ab0290a8166abf836d7a44c1e917f8c0739b04
Josy Santos <112010150+hijoosy@users.noreply.github.com>
2022-10-03 16:39:51 -0300

    Add files via upload

---
+++ exercicio1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio1</title>
+</head>
+<body>
+
+    <h1> Bem Vindo ao programador Web-Mulheres tech!</h1>
+    <?php
+    echo"<h1> Bem Vindo ao programador Web-Mulheres tech!</h1>";
+   
+     //DECLARAÇAO DE VARIÁVEIS
+     $nome = "Ana Julia";
+     $nome = strtoupper($nome);
+     $idade = 18;
+     $salario = 5600.;
+
+     echo"Nome :" .$nome;
+    echo"<br/>Idade :" .$idade;
+    echo"<br/>Salário :" .$salario
+    ?>
+
+</body>
+</html>
